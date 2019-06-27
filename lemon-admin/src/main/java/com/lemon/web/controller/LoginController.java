be433b16ea0a4900c8fb4871ff22abfbbcd8321a
shijianpeng <shijianpeng@xyz.cn>
2019-06-27 10:31:14 +0800

    配置中心

--- lemon-admin/src/main/java/com/lemon/web/controller/LoginController.java
+++ lemon-admin/src/main/java/com/lemon/web/controller/LoginController.java
@@ -1,18 +1,23 @@
 package com.lemon.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sjp 2019/1/10
  */
 @Controller
-@RequestMapping("/admin")
+@RestController
 public class LoginController {
 
-	@RequestMapping("login")
-	public String login() {
-		return "success";
-	}
+    @Value("${version-config}")
+    private String version;
+
+    @GetMapping("/test")
+    public String getVersion() {
+        return this.version;
+    }
 
 }
