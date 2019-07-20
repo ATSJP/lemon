24bcc58c01a1a435ffde2a4507d16c2672c0b179
atsjp <572948072@qq.com>
2019-07-20 15:03:52 +0800

    拆分环境

--- lemon-soa/eureka-server/src/main/java/com/lemon/eureka/server/EurekaServerApplication.java
+++ lemon-soa/eureka-server/src/main/java/com/lemon/eureka/server/EurekaServerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * 微服务注册中心
+ * 
  * @author sjp
  */
 @EnableEurekaServer
